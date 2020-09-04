299584e9bc43d2f88e31f4d6211e2a120c5aa7c3
pawnlord <scruffduff321@gmail.com>
2020-09-04 14:46:47 -0400

    Add general layout of bot Add .gitignore

--- bot.py
+++ bot.py
@@ -14,43 +14,26 @@ slack_events_adapter = seapi.SlackEventAdapter(signing_secret, "/slack/events")
 
 slack_client = WebClient(bot_token)
 
-@slack_events_adapter.on("message")
-def handle_message(event_data):
-    message = event_data["event"]
-    print("message recieved:", message.get("text"))
-    if message.get("subtype") is None and "hello" in message.get('text'):
-        channel = message["channel"]
-        print(channel, type(channel))
-        send_message = bc.create_normal_message("hello", channel)
-        slack_client.chat_postMessage(**send_message)
-
-
-@slack_events_adapter.on("error")
-def error_handler(err):
-    print("ERROR: " + str(err))
-
-
-slack_events_adapter.start(port=3000)
-"""
-trigger = "hello"
-
-@slack_events_adapter.on("message")
+@slack_events_adapter.on("app_mention")
 def on_message(event_data):
-    print("on_message")
     message = event_data["event"]
     print("message recieved: " + message.get("text"))
-    if message.get("subtype") is None and trigger in message.get("text"):
-        channel=message.channel
-        test_message = "hello!"
-        print(channel, "is a", type(channel))
-        
-        slack_client.api_call("chat.postMessage", channel=channel, text=test_message)
+    if message.get("subtype") is None:
+        channel = message["channel"]
+        text = message.get('text')
+        splitup = text.split(' ')
+        if len(splitup) <= 1:
+            send_message = bc.create_normal_message("hello! mention me with `help` for commands.", channel)
+            slack_client.chat_postMessage(**send_message)
+            return
+        handle_command(splitup[1], splitup[1:])    
+
+def handle_command(command, args): 
+    pass
 
 @slack_events_adapter.on("error")
 def error_handler(err):
     print("ERROR: " + str(err))
 
-print("bot running")
-slack_events_adapter.start(port=3000)
 
-"""
+slack_events_adapter.start(port=3000)
